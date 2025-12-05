28796dd75494a1dc6e2bda9404b4f0cb91478b3b
abel malu <abellmalu@gmail.com>
2025-12-05 17:09:44 +0300

    image size updated

--- lib/cafeteria_scanning_screen.dart
+++ lib/cafeteria_scanning_screen.dart
@@ -521,8 +521,14 @@ class ScanResultCard extends StatelessWidget {
               ),
             ],
           ),
-          Text(studentData?['data']['image_url']),
-          Image.network(studentData?['data']['image_url'], fit: BoxFit.cover),
+
+          Image.network(
+            width: 300,
+            height: 300,
+
+            studentData?['data']['image_url'],
+            fit: BoxFit.cover,
+          ),
           const Divider(height: 30, color: kSecondaryTextColor),
           _buildInfoRow('UID:', tagUid ?? 'Unknown Tag', kSecondaryTextColor),
           const SizedBox(height: 10),
